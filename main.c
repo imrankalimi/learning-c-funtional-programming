0ba69afd6844892a1ef2671824f34d1d8e4ec82a
Imran Kalimi <121376069+imrankalimi@users.noreply.github.com>
2023-01-04 12:29:13 +0530

    Hello World Function added

---
+++ main.c
@@ -0,0 +1,21 @@
+// library files
+// main function
+//
+//#include "stdio.h"
+//
+//int main() {
+//    return 0;
+//}
+#include "stdio.h"
+
+
+int sum(int x, int y) {
+    return x +y;
+}
+
+int main() {
+    printf("Hello World\n");
+    printf("%d", sum(2, 6));
+    return 0;
+}
+
